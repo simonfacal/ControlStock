ff02504dc2afbb88b8fc60d4066ea02ea88e3768
Simón Facal <sfacal@mobydigital.com>
2024-01-08 10:24:29 -0300

    creacion de la entidad Cliente

--- src/main/java/com/simonfacal/controlStock/model/Cliente.java
+++ src/main/java/com/simonfacal/controlStock/model/Cliente.java
@@ -1,2 +1,69 @@
-package com.simonfacal.controlStock.model;public class Cliente {
+package com.simonfacal.controlStock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
+    @Size(min=7)
+    private Long dni;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nombre, String apellido, Long dni) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Long getDni() {
+        return dni;
+    }
+
+    public void setDni(Long dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni=" + dni +
+                '}';
+    }
 }
+
