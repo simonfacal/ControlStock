06111296fdbcadfda5e1f456cd99a9e53cd0470f
Simón Facal <sfacal@mobydigital.com>
2024-01-08 08:38:20 -0300

    commit inicial

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+8
+mvnw.cmd,1\f\1f1763f358c257ea3515417352b82b0165647ad1
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/test/java/com/simonfacal/controlStock/ControlStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonfacal.controlStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
