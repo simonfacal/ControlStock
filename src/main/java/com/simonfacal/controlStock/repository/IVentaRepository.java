dd7385a8eb5b21d8801694818e027b2565a6c909
Simón Facal <sfacal@mobydigital.com>
2024-01-08 11:10:12 -0300

    repositorios creados

---
+++ src/main/java/com/simonfacal/controlStock/repository/IVentaRepository.java
@@ -0,0 +1,9 @@
+package com.simonfacal.controlStock.repository;
+
+import com.simonfacal.controlStock.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IVentaRepository extends JpaRepository<Venta,Long> {
+}
